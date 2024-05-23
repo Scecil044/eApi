2bcc60ffe7fb8d599a9ea0741bc92118a4d2f326
Scecil044 <scecil072@gmail.com>
2024-05-23 09:28:21 +0300

    changes to include logges on chat controller, and users controller

--- server/controllers/chat.controller.js
+++ server/controllers/chat.controller.js
@@ -8,7 +8,9 @@ import {
   listAllChats,
   removeMemberFromChat,
 } from "../services/chat.service.js";
+import { createLog } from "../services/log.service.js";
 import { errorHandler } from "../utils/error.js";
+import { logger } from "../utils/winstonLogger.js";
 
 /**
  * Admin controller function to get all chats
@@ -17,22 +19,46 @@ export const getAllChats = async (req, res, next) => {
   try {
     const chats = await listAllChats();
     if (!chats) return next(errorHandler("could not find chats"));
+    const logString = logger.info(
+      `${req.user.userName} accessed the get all chats route`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json(chats);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the get all chats route`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
 export const createChat = async (req, res, next) => {
   try {
     const chat = await createNewChat(req.body);
+    const logString = logger.info(
+      `${req.user.userName} accessed the create new chat route`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json(chat);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the create new chat route`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
 export const updateChat = async (req, res, next) => {
   try {
+    const logString = logger.info(
+      `${req.user.userName} accessed the update chat route for chat id ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the update chat route`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -40,8 +66,16 @@ export const updateChat = async (req, res, next) => {
 export const listChatMembers = async (req, res, next) => {
   try {
     const chatMembers = await getChatMembers(req.params.id);
+    const logString = logger.info(
+      `${req.user.userName} accessed list chat members route for chat id ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json(chatMembers);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the list all chat members route`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -51,8 +85,16 @@ export const deleteChat = async (req, res, next) => {
     const result = await discardChat(req.params.id);
     if (!result)
       return next(errorHandler(400, "something broke down when deleting chat"));
+    const logString = logger.info(
+      `${req.user.userName} accessed delete chat route for chat id ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json("chat deleted");
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the delete chat route`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -60,8 +102,16 @@ export const deleteChat = async (req, res, next) => {
 export const archiveChat = async (req, res, next) => {
   try {
     await addToArchive(req.params.id);
+    const logString = logger.info(
+      `${req.user.userName} accessed archive chat route for chat id ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json("chat archived");
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the archive chat route for chat id: ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -73,8 +123,16 @@ export const addContributor = async (req, res, next) => {
       req.params.memberId
     );
 
+    const logString = logger.info(
+      `${req.user.userName} accessed add chat contributor for chat id ${req.params.chatId}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json(result);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the add chat contributor route for chat ${req.params.chatId}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -85,8 +143,16 @@ export const removeMember = async (req, res, next) => {
       req.params.chatId,
       req.params.memberId
     );
+    const logString = logger.info(
+      `${req.user.userName} accessed archive remove member id ${req.params.memberId} for chat id ${req.params.chatId}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json(result);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the remove member route for chat id: ${req.params.chatId}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -96,8 +162,16 @@ export const deleteMessage = async (req, res, next) => {
       req.params.chatId,
       req.params.messageId
     );
+    const logString = logger.info(
+      `${req.user.userName} accessed delete chat route for chat id ${req.params.chatId}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json("Message deleted!");
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the delete message route for chat id: ${req.params.chatId}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,4 @@
 import { createLog } from "../services/log.service.js";
-import { findRoleByRoleId } from "../services/role.service.js";
 import {
   deactivateUserAccount,
   discardUser,
@@ -42,8 +41,16 @@ export const getUser = async (req, res, next) => {
     const user = await findUserById(req.params.id);
     if (!user) return next(errorHandler(404, "user not found in db!"));
     user.password = undefined;
+    const logString = logger.info(
+      `${req.user.userName} accessed get user route for id ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json(user);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the  get user ${req.params.id} from db`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -55,16 +62,32 @@ export const updateUser = async (req, res, next) => {
       return next(
         errorHandler(400, "An error was encountered, could not update user")
       );
+    const logString = logger.info(
+      `${req.user.userName} accessed get user update user route for id ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json(updatedUser);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the  update user functionality for ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
 export const deleteUser = async (req, res, next) => {
   try {
     await discardUser(req.params.id);
+    const logString = logger.info(
+      `${req.user.userName} accessed get user delete user route for id ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json("User deleted");
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the delete user functionality for ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -76,8 +99,16 @@ export const suspendUser = async (req, res, next) => {
       return next(errorHandler(400, "You do not have Administrator rights!"));
     }
     const updatedUser = await deactivateUserAccount(req.params.id);
+    const logString = logger.info(
+      `${req.user.userName} accessed get user suspend user route for id ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     res.status(200).json(updatedUser);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the suspend user functionality for ${req.params.id}`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -89,11 +120,15 @@ export const getTotalNumberOfTraders = async (req, res, next) => {
   try {
     const traders = await getTradersCount();
     const logString = logger.info(
-      `${req.user.userName} accessed total number of traders`
+      `${req.user.userName} accessed get number of traders route`
     ).transports[0].logString;
     await createLog(req.user.id, logString);
     res.status(200).json(traders);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the get number of traders functionality`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
@@ -105,8 +140,16 @@ export const getTotalNumberOfTraders = async (req, res, next) => {
 export const recentRegistrations = async (req, res, next) => {
   try {
     const recentRegistrationCount = await registrationsWithinTheLastSixMonths();
-    test.status(200).json(recentRegistrationCount);
+    const logString = logger.info(
+      `${req.user.userName} accessed recent registrations route`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
+    res.status(200).json(recentRegistrationCount);
   } catch (error) {
+    const logString = logger.info(
+      `${req.user.userName} unable to access the get recent registrations functionality`
+    ).transports[0].logString;
+    await createLog(req.user.id, logString);
     next(error);
   }
 };
